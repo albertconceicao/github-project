7ecd2ecf63315abbef28274c37c6636fd5c918cb
Albert Conceição <developer.albert@outlook.com>
2023-07-19 15:08:02 -0300

    feat: creating API hook

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,5 +12,24 @@ export default createGlobalStyle`
     color: ${props => props.theme.colors.text};
     font: 400 16px Inter, sans-serif;
   }
+ main {
+  max-width: 80%;
+  margin: auto;
+  section {
+    margin-top: 30vh;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
 
+ }
+
+ .repository {
+  margin-bottom: 20px;
+  h1 {
+    font-size: 24px;
+  }
+  p {
+    font-size: 16px;
+  }
+ }
 `;
